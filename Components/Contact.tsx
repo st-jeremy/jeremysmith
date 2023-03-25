e355ee6e301fd5028f3cc49eeb600f59ead5c58d
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-26 00:55:57 +0100

    added footer component

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -1,7 +1,23 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
 const Contact = () => {
   return ( 
-    <>
-    </>
+    <Box px={'1rem'} py={'3rem'} minHeight={'100vh'}>
+      <Heading>Contact</Heading>
+      <Text>
+        Available for select opportunities <br />
+
+        Have an exciting project you need help with or collaboration on?
+        <br />
+
+        Contact me via 
+        <a href="mailto:hello@tamal.dev">hello@tamal.dev</a> <br />
+        <a href="http://wa.me/2349053292009">WhatsApp Messenger</a> <br />
+        <a href="http://">LinkedIn</a> <br />
+        <a href="http://">Github</a> <br />
+        <a href="tel:+2349053292009">+234 905 329 2009</a>
+
+      </Text>
+    </Box>
    );
 }
  
---
+++ Components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return ( 
+    <Box>
+      
+    </Box>
+   );
+}
+ 
+export default Footer;
