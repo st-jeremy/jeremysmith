385a1e063f6c78e926798410d14b4077a1d361ba
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-26 00:58:18 +0100

    footer updates

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,9 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Button, Input, Divider } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return ( 
-    <Box>
-      
+    <Box bgColor={'#0D101C'} color={'white'}>
+    
+      <Divider color={'#0C37AA'} w={'90%'} m={'auto'} />
+
+      <Text fontSize={'14px'} textAlign={'center'} p={'10px 0'}>
+        Copyright © {new Date().getFullYear()}- sonergy.io
+      </Text>
     </Box>
    );
 }
