d19dda1cc0f33f26caa13c97f908b671546b60d2
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-25 17:40:34 +0100

    fixes

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,8 +1,11 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 
 const Hero = () => {
   return ( 
-    <Box>
+    <Box height={'90vh'}>
+      <Heading>
+        With a mission for good user-experience centric purpose, we are able to turn ideation to reality
+      </Heading>
       
     </Box>
    );
