4fa08eac3a10791f91e664db9177017760c9d5f3
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-25 23:53:32 +0100

    updates

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -2,9 +2,17 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 
 const Hero = () => {
   return ( 
-    <Box px={'1rem'} py={'1rem'} height={'90vh'}>
+    <Box px={'1rem'} py={'1rem'} height={'92vh'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'}>
+      <Heading fontSize={'50px'}>
+        JEREMY <br /> SMITH 
+      </Heading>
+
+      <Text>
+        Front-End Developer
+      </Text>
+
       <Text >
-        With a mission for good user-experience centric purpose, we are able to turn ideation to reality
+        ...a mission for good user-experience centric purpose, we are able to turn ideation to reality
       </Text>
     </Box>
    );
