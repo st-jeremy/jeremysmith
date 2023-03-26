05b48bce95131bd799e10f0e8e0fe74b3391f0dc
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-26 01:24:14 +0100

    fixes

--- Components/Tech.tsx
+++ Components/Tech.tsx
@@ -17,50 +17,52 @@ const TechImage = (props: any) => {
  
 const Tech = () => {
   return ( 
-    <Box bgColor={'white'} py={'2rem'} px={'3rem'} height={'100vh'}>
+    <Box bgColor={'white'} py={'2rem'} px={'3rem'} minHeight={'100vh'}>
       <Heading>Tech Stack</Heading>
-      <TechImage 
-        src='/HTML5.png'
-        alt='HTML'
-      />
-      <TechImage 
-        src='/CSS.png'
-        alt='CSS'
-      />
-      <TechImage 
-        src='/JavaScript.png'
-        alt='JavaScript'
-      />
-      <TechImage 
-        src='/TypeScript.png'
-        alt='TypeScript'
-      />
-      <TechImage 
-        src='/react.png'
-        alt='React'
-      />
-      <TechImage 
-        src='/Nextjs.png'
-        alt='Next.JS'
-      />
+      <Box>
+        <TechImage 
+          src='/HTML5.png'
+          alt='HTML'
+        />
+        <TechImage 
+          src='/CSS.png'
+          alt='CSS'
+        />
+        <TechImage 
+          src='/JavaScript.png'
+          alt='JavaScript'
+        />
+        <TechImage 
+          src='/TypeScript.png'
+          alt='TypeScript'
+        />
+        <TechImage 
+          src='/react.png'
+          alt='React'
+        />
+        <TechImage 
+          src='/Nextjs.png'
+          alt='Next.JS'
+        />
 
-      <TechImage 
-        src='/thirteen.svg'
-        alt='Material UI'
-      />
-      <TechImage 
-        src='/chakra.png.svg'
-        alt='Chakra UI'
-      />
-      <TechImage 
-        src='/thirteen.svg'
-        alt='Tailwind'
-      />
+        <TechImage 
+          src='/thirteen.svg'
+          alt='Material UI'
+        />
+        <TechImage 
+          src='/chakra.png'
+          alt='Chakra UI'
+        />
+        <TechImage 
+          src='/tailwind.png'
+          alt='Tailwind'
+        />
 
-      <TechImage 
-        src='/thirteen.svg'
-        alt='HTML'
-      />
+        <TechImage 
+          src='/thirteen.svg'
+          alt='HTML'
+        />
+      </Box>
 
     </Box>
    );
