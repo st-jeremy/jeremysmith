4e63a7eabb436fd0fd2df01362ba458d5473a3e4
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-27 22:39:14 +0100

    fixes

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -2,17 +2,17 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 
 const Hero = () => {
   return ( 
-    <Box px={'1rem'} py={'1rem'} height={'92vh'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'} id={'hero'}>
-      <Heading fontSize={'50px'}>
+    <Box px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '5rem'}} py={'1rem'} height={'92vh'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'} id={'hero'}>
+      <Heading fontSize={{base:'50px', lg: '64px'}}>
         JEREMY <br /> SMITH 
       </Heading>
 
-      <Text>
-        Front-End Developer
-      </Text>
+      <Heading fontSize={{base:'20px', lg:'30px'}}>
+        FRONT-END DEVELOPER
+      </Heading>
 
-      <Text >
-        ...a mission for turning designs into great user-experience with an encompassing user-centric approach
+      <Text fontSize={{lg: '24px'}}>
+        ...a mission for turning designs, with an encompassing user-centric approach to deliver a great experience.
       </Text>
     </Box>
    );
