c32d5e5d5722bbc4f4d786fbe8bd28211f6c4394
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-27 22:23:31 +0100

    fixes

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -10,13 +10,13 @@ import Navlinks from "./Navlinks";
 const Navbar = () => {
  
   return ( 
-    <Box position={'fixed'} width={{base: '100%'}}  zIndex={'1000'} px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '5rem'}} py={{base: '.6rem', sm: '1rem', md:'1rem'}} borderBottom={'1px solid red'} bgColor={'black'} display={'flex'} justifyItems={'center'} alignItems={'center'}>
+    <Box position={'fixed'} width={{base: '100%'}}  zIndex={'1000'} px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '5rem'}} py={{base: '.6rem', sm: '1rem', md:'1rem'}} borderBottom={'1px solid red'} bgColor={'black'} display={'flex'} maxWidth={'1440px'} >
  
       <Heading fontSize={'25px'} width={'fit-content'} m={0}>Jeremy_Smith</Heading>
 
       <Menu />
 
-      <Box display={{base: 'none', md:'flex'}} >
+      <Box display={{base: 'none', md:'flex'}} position={{base: 'absolute'}} right={{md: '3rem'}} >
         <Navlinks 
           display='flex'
         />
