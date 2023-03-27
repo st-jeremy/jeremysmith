71b2242ceb4cb2858512d0edb8abe87740cd46e9
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-27 14:21:59 +0100

    fixes

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -12,7 +12,7 @@ import Navlinks from "./Navlinks";
 const Navbar = () => {
  
   return ( 
-    <Box px={'1rem'} py={'.5rem'} borderBottom={'1px solid red'}>
+    <Box px={{base: '1rem', sm: '1.5rem'}} py={{base: '.5rem'}} borderBottom={'1px solid red'}>
       
       <Box _hover={{color: 'red', textDecor: 'underline'}}>
         <h3>Jeremy_Smith</h3>
