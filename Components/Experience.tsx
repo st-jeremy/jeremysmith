3e0f398406412d7e9c81554356cb4dd657223243
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-28 10:02:01 +0100

    fixes

--- Components/Experience.tsx
+++ Components/Experience.tsx
@@ -5,50 +5,57 @@ import {
   AccordionPanel,
   AccordionIcon,
   Box,
-  Heading
+  Heading,
+  Text
   
 } from '@chakra-ui/react'
 
+const ExpAccordion = (props:any) => {
+  return ( 
+    <Box mt={'2rem'} p={0} borderRadius={'20px'} background='black'>
+        <Accordion defaultIndex={[0]} reduceMotion display={'flex'} flexDirection={{base:'column', lg:'row'}} >
+          <AccordionItem color={'white'} bgColor={props.bgColor} p={'1rem'} display={{base:'block', lg:'flex'}} borderRadius={{base:0, lg:'20px 0 0 0 20px'}} borderTop={'1px solid blue'} border={{ lg:'none'}} pb={{lg:'5rem'}} pt={{lg:'5rem'}} >
+            <h2>
+              <AccordionButton mt={{lg:'50%'}} pt={{lg:'0%'}}>
+                <Box as="span" flex='1' textAlign={{base:'center', lg:'left'}} minW={'4rem'}>
+                  <Heading  fontSize={{base:'18px', md:'20px'}} color={'white'}>
+                    {props.title}
+                  </Heading>
+                  <Text>{props.text}</Text>
+                </Box>
+              </AccordionButton>
+            </h2>
+
+            <AccordionPanel pb={4} fontSize={'16px'} borderLeft={{lg:'2px solid black'}}>
+              {props.about}
+              <li>{props.line1}</li>
+              <li>{props.line2}</li>
+              <li>{props.line3}</li>
+            </AccordionPanel>
+          </AccordionItem>
+      </Accordion>
+    </Box>
+   );
+};
+ 
+
 const Experience = () => {
   return ( 
-    <Box px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} py={'3rem'} minH={'100vh'} textAlign={'center'}>
-      <Heading textAlign={'center'} mb={'1rem'}>Experience</Heading>
-    
-      <Accordion defaultIndex={[0]}>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex='1' textAlign='left'>
-                Section 1 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
+    <Box background= 'black' color={'whiteAlpha.600'} textAlign={'left'} padding={{base:'2rem 1rem', md:'10rem 4rem 4rem', lg:'10rem 6rem 4rem'}} minHeight={{lg:'800px'}} id={'roadmap'} >
+      <Heading fontSize={{base:'24px', md:'40px'}} color={'white'}>Experience</Heading>
+      <Text fontSize={{base:'16px', md:'18px'}}>See spaces where I have worked while reaching my goal</Text>
 
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex='1' textAlign='left'>
-                Section 2 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
+      <Box mt={'2rem'} p={0} borderRadius={'20px'} background='rgba(10, 47, 146, 0.2)'>
+        <ExpAccordion 
+          bgColor='grey'
+          title='Implementation Specialist'
+          text='Sendme.ng Jun 2022 – present'
+          about='Sendme.ng (YC W22) is a startup backed by Y Combinators and Voltron Capital, with the goal of decentralizing the distribution of animal protein from production through to consumers using artificial intelligence and data analytics'
+          line1='Collaborated with clients to understand their needs and requirements, and provided recommendations for design         and functionality'
+          line2=''
+          line3=''
+        />
+      </Box>
     </Box>
    );
 }
