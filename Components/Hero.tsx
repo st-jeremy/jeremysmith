93fd23b07ff4d1da0cc3020b31a4597d8a02b68f
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-28 12:26:16 +0100

    animation

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Box, Heading, Text, Progress } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return ( 
@@ -7,10 +8,17 @@ const Hero = () => {
       <Heading fontSize={{base:'50px', lg: '64px'}}>
         JEREMY <br /> SMITH 
       </Heading>
+      {/* <motion.div
+    animate={{
+      scale: [1, 2, 2, 1, 1],
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+    }}> */}
 
       <Heading fontSize={{base:'20px', lg:'30px'}}>
         FRONT-END DEVELOPER
       </Heading>
+      {/* </motion.div> */}
 
       <Text fontSize={{lg: '24px'}}>
         ...a mission for turning designs, with an encompassing user-centric approach to deliver a great experience.
--- Components/Menu.tsx
+++ Components/Menu.tsx
@@ -8,18 +8,18 @@ import {
   Box,
   Text,
   useDisclosure,
-  Divider,
-  Button
 } from '@chakra-ui/react'
 import React from 'react';
 import { BsMenuApp } from 'react-icons/bs'
 import Navlinks from "./Navlinks";
 import styles from '@/styles/Home.module.css'
+import { motion } from "framer-motion"
 
 const Menu = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   
   return ( 
+    
     <Box display={{md: 'none'}}>
       <Box position={'absolute'} right={{base:'1.25rem', sm: '1.5rem', md: '2rem', lg: '5rem'}} top={{base: '1rem', sm:'1.2rem'}} className={styles.btn}>
         <BsMenuApp onClick={onOpen}/>
@@ -31,7 +31,11 @@ const Menu = () => {
         onClose={onClose}
       >
         <DrawerOverlay/>
-
+        <motion.div
+    initial={{ opacity: 0, scale: 0.5 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.5 }}
+    >
         <DrawerContent bgColor={'blackAlpha.900'} color={'white'}>
           <DrawerCloseButton onClick={onClose} border={'none'} />
           <DrawerHeader pl={'2rem'}>
@@ -48,6 +52,8 @@ const Menu = () => {
           </DrawerBody>
          
         </DrawerContent>
+
+    </motion.div>
       </Drawer>
     </Box>
    );
