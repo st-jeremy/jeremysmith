cdd6be04753856801137a6af896d3f69228bdcd5
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-29 22:12:18 +0100

    fixes

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -29,27 +29,27 @@ const Hero = () => {
       dragTransition={{ bounceStiffness: 600, bounceDamping: 50 }} 
       dragPropagation
     >
-      <Box px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '4rem'}} pt={'5rem'} height={{base:'fit-content', lg:'95vh'}} position={'sticky'}  top={'12rem'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'}  width={{base: '100%', md: '60%'}}  id={'hero'}>
+      <Box px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '4rem'}} pt={'8rem'} height={{base:'fit-content', lg:'95vh'}} position={'sticky'}  top={'12rem'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'}  width={{base: '100%', md: '60%'}}  id={'hero'}>
 
-        <Heading fontSize={{base:'40px', lg: '64px'}} textAlign={'left'}>Hello, I am <br /></Heading>
+        <Heading fontSize={{base:'24px', lg: '64px'}} textAlign={'left'}>Hello, I am</Heading>
 
-        <Heading color={'red'} textShadow={'2px solid white'}>JEREMY SMITH </Heading>
+        <Heading fontSize={{base:'35px', lg: '64px'}} color={'red'} textShadow={'2px solid white'}>JEREMY SMITH </Heading>
         
-        <Heading fontSize={{base:'40px', lg: '54px'}} textAlign={'left'}>a Front-End Developer</Heading>
+        <Heading fontSize={{base:'20px', lg: '54px'}} textAlign={'left'}>a Front-End Developer</Heading>
 
-        <Text fontSize={{lg: '24px'}} textAlign={'left'}>
+        <Text fontSize={{base:'16px', lg: '24px'}} textAlign={{base:'center', md:'left'}} mb={'1rem'}>
           ...a mission for turning designs, with an encompassing user-centric approach to deliver a great experience.
         </Text>
 
-        <br />
         <CustomBtn 
           name='Contact me'
           link='#contact'
           rightIcon={<MdArrowForward />}
           bgColor='red'
+          width=''
         />
 
-      <Box position={'absolute'} left={1}>
+      <Box position={'absolute'} left={1} display={{base:'none'}}>
         <Text textDecorationStyle={'revert'}>Scroll down</Text>
       </Box>
     </Box>
