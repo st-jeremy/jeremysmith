294fb9c3ddbb62d9d169293c6018618227b7f53b
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-29 13:18:24 +0100

    fixes

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
   return ( 
     <Box>
     <motion.svg
-      width='100%'
+      width='300'
       height="400"
       viewBox="0 10 600 600"
       initial="hidden"
@@ -37,8 +37,8 @@ const Hero = () => {
         custom={1}
       />
        <motion.rect className={styles.circle}
-        width="140"
-        height="140"
+        width="90"
+        height="170"
         x="410"
         y="430"
         rx="20"
@@ -47,7 +47,7 @@ const Hero = () => {
         custom={2}
       />
        </motion.svg>
-      <Box px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} pt={'.1rem'} height={{base:'fit-content', lg:'95vh'}} position={'sticky'}  top={'12rem'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'} id={'hero'}>
+      <Box mt={'-8rem'} px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} pt={'.1rem'} height={{base:'fit-content', lg:'95vh'}} position={'sticky'}  top={'12rem'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'} id={'hero'}>
         
         <Heading fontSize={{base:'50px', lg: '64px'}}>
           JEREMY <br /> SMITH 
