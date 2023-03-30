eb605e3d47550e2e931d9ce69c5bc1b39316a2b9
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-30 18:32:53 +0100

    fixes

--- Components/Experience.tsx
+++ Components/Experience.tsx
@@ -17,16 +17,18 @@ const ExpAccordion = (props:any) => {
             <h2>
               <AccordionButton mt={{lg:'50%'}} pt={{lg:'0%'}}>
                 <Box as="span" flex='1' textAlign={{base:'center', lg:'left'}} minW={'4rem'}>
-                  <Heading  fontSize={{base:'18px', md:'20px'}} color={'white'}>
+                  <Heading fontFamily={'fantasy'} fontSize={{base:'25px', md:'30px'}}>
                     {props.title}
                   </Heading>
-                  <Text>{props.text}</Text>
+                  <Text>{props.post}</Text>
+                  <Text>{props.date}</Text>
                 </Box>
               </AccordionButton>
             </h2>
 
-            <AccordionPanel pb={4} fontSize={'16px'} borderLeft={{lg:'2px solid black'}}>
+            <AccordionPanel p={'1rem 0 0 0'} fontSize={'16px'} borderLeft={{lg:'2px solid black'}}>
               {props.about}
+              <ul>&nbsp;</ul>
               <li>{props.line1}</li>
               <li>{props.line2}</li>
               <li>{props.line3}</li>
@@ -40,16 +42,17 @@ const ExpAccordion = (props:any) => {
 
 const Experience = () => {
   return ( 
-    <Box textAlign={'left'} padding={{base:'2rem 1rem', md:'5rem 4rem 4rem', lg:'10rem 6rem 4rem'}} minHeight={{lg:'800px'}} id={'experience'} >
-      <Heading fontSize={{base:'24px', md:'40px'}} >Experience</Heading>
-      <Text fontSize={{base:'16px', md:'18px'}}>See spaces where I have worked while reaching my goal</Text>
+    <Box textAlign={'center'} padding={{base:'2rem 1rem', md:'5rem 4rem 4rem', lg:'10rem 6rem 4rem'}} minHeight={{lg:'800px'}} id={'experience'} >
+      <Heading fontSize={{base:'30px', md:'40px'}} fontWeight={900} fontFamily={'fantasy'} mb={5}>Experience</Heading>
+      <Text fontSize={{base:'16px', md:'18px'}}>Spaces where I have worked.</Text>
 
       <Box mt={'1rem'} p={0} borderRadius={'20px'}>
         <Accordion defaultIndex={[0]} reduceMotion display={'flex'} flexDirection={{base:'column', lg:'row'}} >
         <ExpAccordion 
           // bgColor='grey'
           title='Frontend Developer'
-          text='Freelance Nov 2022 – Present'
+          post='Freelance'
+          date='Nov 2022 – Present'
           about='Collaborated with other devs in building landing pages, ecommerce website, blog etc'
           line1='Implement several websites, and web applications: ecommerce and landing pages from concept to deployment'
           line2='Implement responsive design techniques to ensure websites were optimized for mobile devices.'
@@ -60,7 +63,8 @@ const Experience = () => {
         <ExpAccordion 
           // bgColor='grey'
           title='Implementation Specialist'
-          text='Sendme.ng Jun 2022 – present'
+          post='Sendme.ng'
+          date='Jun 2022 – present'
           about='Sendme.ng (YC W22) is a startup backed by Y Combinators and Voltron Capital, with the goal of decentralizing the distribution of animal protein from production through to consumers using artificial intelligence and data analytics'
           line1='Collaborate with clients to understand their needs and requirements, and provided recommendations for design         and functionality'
           line2='Test software, debugged issues, and supported bug resolution on apps and software'
