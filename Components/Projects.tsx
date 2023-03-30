8c4f02de922dc4a6520d20fed8d9eeba84c64181
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-30 17:50:58 +0100

    fixes

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -14,8 +14,8 @@ import styles from '@/styles/Home.module.css'
 
 const ProjectCard = (props:any) => {
   return ( 
-    <Card width='350px' height={'430px'} alignItems={'center'} mb={'1rem'} pb={'1rem'} className={styles.projectCard}>
-      <Box width={{base:'100%'}}>
+    <Card width='350px' height={'460px'} alignItems={'center'} mb={'1rem'} pb={'1rem'} className={styles.projectCard}>
+      <Box width={{base:'100%'}} height={350}>
         <Image
           src={props.src}
           alt={props.alt}
@@ -24,7 +24,7 @@ const ProjectCard = (props:any) => {
         />
       </Box>
 
-      <Box bgColor={'linear-gradient(to bottom, rgba(255,0,0,0), rgba(255,0,0,1))'}>
+      <Box bgColor={'linear-gradient(to bottom, rgba(255,0,0,0), rgba(255,0,0,1))'} mt={'-9rem'}>
         <Stack spacing='.1' p={'1rem'} >
           <Heading size='md'>{props.heading}</Heading>
           <Text>{props.description}</Text>
@@ -32,38 +32,39 @@ const ProjectCard = (props:any) => {
             {props.category}
           </Text>
           <Box display={'flex'} textAlign={'center'} gap={'.3rem'} pb={'.3rem'}>
-            <Badge>{props.badge1}</Badge>
-            <Badge>{props.badge2}</Badge>
-            <Badge>{props.badge3}</Badge>
+            <Badge bgColor={'grey'}>{props.badge1}</Badge>
+            <Badge bgColor={'blue'}>{props.badge2}</Badge>
+            <Badge bgColor={'red.800'}>{props.badge3}</Badge>
           </Box>
           <Box display={'flex'} textAlign={'center'} gap={'.3rem'}>
-            <Badge>{props.badge4}</Badge>
-            <Badge>{props.badge5}</Badge>
+            <Badge bgColor={'green'}>{props.badge4}</Badge>
+            <Badge bgColor={'blue.300'}>{props.badge5}</Badge>
           </Box>
         </Stack>
 
         <ButtonGroup position={'absolute'} bottom={'1rem'} left={'2.4rem'}>
-        <motion.button
-      whileHover={{
-        scale: 1.2,
-        transition: { duration: 1 },
-      }}
-      whileTap={{ scale: 0.9 }}
-    >
-          <Button variant='outline' colorScheme='black' onClick={props.repoLink} fontSize={'14px'}>
-            View Repository
-          </Button>
-          </motion.button>
           <motion.button
-      whileHover={{
-        scale: 1.2,
-        transition: { duration: 1 },
-      }}
-      whileTap={{ scale: 0.9 }}
-    >
-          <Button variant='outline' colorScheme='black' onClick={props.link} fontSize={'16px'}>
-            Visit 
-          </Button>
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <Button variant='outline' colorScheme='black' onClick={props.repoLink} fontSize={'14px'} height={8}>
+              View Repository
+            </Button>
+          </motion.button>
+
+          <motion.button
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <Button variant='outline' colorScheme='black' onClick={props.link} fontSize={'16px'} height={8}>
+              Visit 
+            </Button>
           </motion.button>
         </ButtonGroup>
       </Box>
