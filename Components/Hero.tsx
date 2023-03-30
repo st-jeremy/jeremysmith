31d919fb34c55d31dcc984fea5b13b26792bc300
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-30 18:07:28 +0100

    fixes

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -14,21 +14,21 @@ const Hero = () => {
       dragTransition={{ bounceStiffness: 600, bounceDamping: 50 }} 
       dragPropagation
     >
-      <Box px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '4rem'}} pt={'8rem'} height={{base:'fit-content', lg:'95vh'}} position={'sticky'}  top={'12rem'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'}  width={{base: '100%', md: '60%'}}  id={'hero'}>
+      <Box px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '4rem'}} pt={'7rem'} height={{base:'fit-content', lg:'95vh'}} position={'sticky'}  top={'12rem'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'}  width={{base: '100%', md: '60%'}}  id={'hero'}>
 
-        <Heading fontSize={{base:'24px', lg: '64px'}} textAlign={'left'}>Hello, I am</Heading>
+        <Heading fontFamily={'fantasy'} fontSize={{base:'24px', lg: '64px'}} textAlign={'left'}>Hello, I am</Heading>
 
-        <Heading fontSize={{base:'35px', lg: '64px'}} color={'red'} textShadow={'2px solid white'}>JEREMY SMITH </Heading>
+        <Heading fontFamily={'fantasy'} fontSize={{base:'50px', lg: '64px'}} color={'blue.900'} textShadow={'2px solid white'}>JEREMY SMITH </Heading>
         
-        <Heading fontSize={{base:'20px', lg: '54px'}} textAlign={'left'}>a Front-End Developer</Heading>
+        <Heading fontFamily={'fantasy'} fontSize={{base:'20px', lg: '54px'}} textAlign={'left'}>a Front-End Developer</Heading>
 
         <Text fontSize={{base:'16px', lg: '24px'}} textAlign={{base:'center', md:'left'}} mb={'1rem'}>
-          ...a mission for turning designs, with an encompassing user-centric approach to deliver a great experience.
+          ...with a mission of turning designs delivering great experience, through user-centric approach.
         </Text>
 
 
-        <Button rightIcon={<MdArrowForward />} color={'red'}>
-          <a href="http://#contact">
+        <Button rightIcon={<MdArrowForward />} color={'blue.900'}>
+          <a href="#contact">
             Contact me
           </a>
         </Button>
