59f4964134f165cb34ffe29da7f669e35ea0d360
Jeremy Smith <st.jeremy1@gmail.com>
2023-03-30 21:03:34 +0100

    fixes

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -14,32 +14,32 @@ const Hero = () => {
       dragTransition={{ bounceStiffness: 600, bounceDamping: 50 }} 
       dragPropagation
     >
-      <Box px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '4rem'}} pt={'7rem'} height={{base:'fit-content', lg:'95vh'}} position={'sticky'}  top={'12rem'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} gap={'1rem'}  width={{base: '100%', md: '60%'}}  id={'hero'}>
+      <Box px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} pt={'7rem'} height={{base:'fit-content', lg:'fit-content'}} position={'sticky'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={{base:'center', md: 'left'}} gap={'1rem'} width={{base: '100%', md: '60%'}} id={'hero'}>
 
-        <Heading fontFamily={'fantasy'} fontSize={{base:'24px', lg: '64px'}} textAlign={'left'}>Hello, I am</Heading>
+        <Box textAlign={{base:'center', md: 'left'}}>
+          <Heading fontFamily={'fantasy'} fontSize={{base:'24px', lg: '64px'}}>Hello, I am</Heading>
 
-        <Heading fontFamily={'fantasy'} fontSize={{base:'50px', lg: '64px'}} color={'blue.900'} textShadow={'2px solid white'}>JEREMY SMITH </Heading>
-        
-        <Heading fontFamily={'fantasy'} fontSize={{base:'20px', lg: '54px'}} textAlign={'left'}>a Front-End Developer</Heading>
+          <Heading fontFamily={'fantasy'} fontSize={{base:'50px', lg: '64px'}} color={'blue.900'} textShadow={'2px solid white'}>JEREMY SMITH </Heading>
+          
+          <Heading fontFamily={'fantasy'} fontSize={{base:'20px', lg: '54px'}}>a Front-End Developer</Heading>
 
-        <Text fontSize={{base:'16px', lg: '24px'}} textAlign={{base:'center', md:'left'}} mb={'1rem'}>
-          ...with a mission of turning designs delivering great experience, through user-centric approach.
-        </Text>
+          <Text fontSize={{base:'16px', lg: '24px'}} mb={'1rem'}>
+            ...with a mission of turning designs delivering great experience, through user-centric approach.
+          </Text>
 
-
-        <Button rightIcon={<MdArrowForward />} color={'blue.900'}>
-          <a href="#contact">
-            Contact me
-          </a>
-        </Button>
-
-      <Box position={'absolute'} left={1} display={{base:'none'}}>
-        <Text textDecorationStyle={'revert'}>Scroll down</Text>
+          <Button rightIcon={<MdArrowForward />} color={'blue.900'}>
+            <a href="#contact">
+              Contact me
+            </a>
+          </Button>
+        </Box>
       </Box>
-    </Box>
 
+      {/* <Box position={'absolute'} left={0} top={'12rem'}>
+        <Text className={styles.scrollDown} bgColor={'beige'}>Scroll down</Text>
+      </Box> */}
     </motion.div>
-   );
+  );
 }
- 
+
 export default Hero;
