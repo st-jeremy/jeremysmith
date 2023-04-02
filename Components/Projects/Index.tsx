0f72167e22227a210f581603a5b373e10372c066
Jeremy Smith <st.jeremy1@gmail.com>
2023-04-02 16:30:00 +0100

    fixes

--- Components/Projects/Index.tsx
+++ Components/Projects/Index.tsx
@@ -1,28 +1,18 @@
 import { 
-  Card, 
-  Badge,
   Box,
-  Stack,
-  Button,
-  Text,
-  ButtonGroup,
   Heading
 } from '@chakra-ui/react'
-import Image from 'next/image';
-import { motion } from 'framer-motion'
-import styles from '@/styles/Home.module.css'
-import MultiCarousel from './ProjectCarousel';
-import ProjectCard from './ProjectCard';
-import LiveProjects from './LiveProjects';
+import ProjectCarousel from './ProjectCarousel';
 
  
 
 const Projects = () => {
   return ( 
-    <Box  px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} py={{base:'3rem', md:'5rem', lg: '7rem'}} minH={'100vh'} textAlign={'center'} id={'projects'}>
-      <Heading pb={'1rem'}>Projects</Heading>
+    <Box  px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} py={{base:'3rem', md:'5rem', lg: '7rem'}} minH={'100vh'} textAlign={'center'} bgColor={'blue.100'} id={'projects'}>
 
-      <LiveProjects />
+      <Heading fontFamily={'fantasy'}>Projects</Heading>
+
+      <ProjectCarousel />
 
       {/* <Heading fontFamily={'fantasy'} p={'4rem'}>Still cooking in the pot...</Heading>
       <Box display={'grid'} gridTemplateColumns={'auto auto auto'}>
