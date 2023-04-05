de4d3eee5fd10f751052f665f8a3d0abe17b30e4
Jeremy Smith <st.jeremy1@gmail.com>
2023-04-05 22:40:45 +0100

    fixes

--- Components/Tech.tsx
+++ Components/Tech.tsx
@@ -19,83 +19,81 @@ const TechImage = (props: any) => {
 const Tech = () => {
   return ( 
     <motion.div
-    initial={{marginTop: '-3rem'}}
-  whileInView={{marginTop: '0'}}
-  viewport={{ once: true }}
-  transition={spring}
+      initial={{marginTop: '-3rem'}}
+      whileInView={{marginTop: '0'}}
+      viewport={{ once: true }}
+      transition={spring}
     >
+      <Box py={{base:'2.5rem', }}  px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} textAlign={'center'} minHeight={'50vh'} id={'tech'}>
+        <Heading textAlign={'center'}>Tech Stack</Heading>
+        <Text pb={'1rem'}>Tech worked with over time. Skilled at using the following software</Text>
 
+        <Box 
+          display={{base:'grid'}} 
+          gap={'1rem'} 
+          gridTemplateColumns={{base:'repeat(3, minmax(0, 1fr));'}} 
+        >
+          <TechImage 
+            src='/HTML5.png'
+            alt='HTML'
+          />
+          <TechImage 
+            src='/CSS.png'
+            alt='CSS'
+          />
+          <TechImage 
+            src='/JavaScript.png'
+            alt='JavaScript'
+          />
+          <TechImage 
+            src='/TypeScript.png'
+            alt='TypeScript'
+          />
+          <TechImage 
+            src='/react.png'
+            alt='React'
+          />
+          <TechImage 
+            src='/react.png'
+            alt='Next.JS'
+          />
 
-    <Box py={{base:'2.5rem', }}  px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} textAlign={'center'} minHeight={'50vh'} id={'tech'}>
-      <Heading textAlign={'center'}>Tech Stack</Heading>
-      <Text pb={'1rem'}>Tech worked with over time. Skilled at using the following software</Text>
+          <TechImage 
+            src='/react.png'
+            alt='Material UI'
+          />
+          <TechImage 
+            src='/chakra.png'
+            alt='Chakra UI'
+          />
+          <TechImage 
+            src='/tailwind.png'
+            alt='Tailwind'
+          />
+          <TechImage 
+            src='/tailwind.png'
+            alt='git'
+          />
+          <TechImage 
+            src='/tailwind.png'
+            alt='Bootstrap'
+          />
+          <TechImage 
+            src='/tailwind.png'
+            alt='SASS'
+          />
+          <TechImage 
+            src='/tailwind.png'
+            alt='Jira'
+          />
+          <TechImage 
+            src='/tailwind.png'
+            alt='Redux'
+          />
 
-      <Box 
-        display={{base:'grid'}} 
-        gap={'1rem'} 
-        gridTemplateColumns={{base:'repeat(3, minmax(0, 1fr));'}} 
-      >
-        <TechImage 
-          src='/HTML5.png'
-          alt='HTML'
-        />
-        <TechImage 
-          src='/CSS.png'
-          alt='CSS'
-        />
-        <TechImage 
-          src='/JavaScript.png'
-          alt='JavaScript'
-        />
-        <TechImage 
-          src='/TypeScript.png'
-          alt='TypeScript'
-        />
-        <TechImage 
-          src='/react.png'
-          alt='React'
-        />
-        <TechImage 
-          src='/react.png'
-          alt='Next.JS'
-        />
-
-        <TechImage 
-          src='/react.png'
-          alt='Material UI'
-        />
-        <TechImage 
-          src='/chakra.png'
-          alt='Chakra UI'
-        />
-        <TechImage 
-          src='/tailwind.png'
-          alt='Tailwind'
-        />
-        <TechImage 
-          src='/tailwind.png'
-          alt='git'
-        />
-        <TechImage 
-          src='/tailwind.png'
-          alt='Bootstrap'
-        />
-        <TechImage 
-          src='/tailwind.png'
-          alt='SASS'
-        />
-        <TechImage 
-          src='/tailwind.png'
-          alt='Jira'
-        />
-        <TechImage 
-          src='/tailwind.png'
-          alt='Redux'
-        />
+        </Box>
 
       </Box>
-
-    </Box>
     </motion.div>
    );
 }
