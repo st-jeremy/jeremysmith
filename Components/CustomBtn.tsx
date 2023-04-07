460cfc6ef8f97466fb4cc69c5d95bed8d1f64194
Jeremy Smith <st.jeremy1@gmail.com>
2023-04-07 19:30:15 +0100

    fixes

--- Components/CustomBtn.tsx
+++ Components/CustomBtn.tsx
@@ -5,31 +5,32 @@ const CustomBtn = (props: any) => {
   
   return ( 
     <Box width={{md:'fit-content'}}>
-    <motion.button
-      whileHover={{
-        scale: 1.2,
-        transition: { duration: 1 },
-      }}
-      whileTap={{ scale: 0.9 }}
-    >
-      <Button 
-        variant={'ghost'} 
-        border={'none'} 
-        width={{base: '11rem', md:'fit-content'}}
-        height={'fit-content'}
-        leftIcon={props.icon} 
-        p={'1.5'}
-        justifyContent='left'
-        _hover={{color:'black', bgColor:'white'}}
-        _visited={{color: 'grey', }}
-        bgColor={props.bgColor}
-        rightIcon={props.rightIcon}
+      <motion.button
+        whileHover={{
+          scale: 1.2,
+          transition: { duration: 1 },
+        }}
+        whileTap={{ scale: 0.9 }}
       >
-        <a href={props.link}>
-          {props.name}
-        </a>
-      </Button>
-    </motion.button>
+        <Button 
+          variant={'ghost'} 
+          border={'none'} 
+          width={{base: '11rem', md:'fit-content'}}
+          height={'fit-content'}
+          leftIcon={props.icon} 
+          p={'1.5'}
+          justifyContent='left'
+          _hover={{color:'black', bgColor:'white'}}
+          _visited={{color: 'grey', }}
+          bgColor={props.bgColor}
+          rightIcon={props.rightIcon}
+          fontSize={{xl: '25px'}}
+        >
+          <a href={props.link}>
+            {props.name}
+          </a>
+        </Button>
+      </motion.button>
     </Box>
    );
 }
