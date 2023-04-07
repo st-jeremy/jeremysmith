54ae73a54e9698155c460ba3fe0751ee44b4e5d2
Jeremy Smith <st.jeremy1@gmail.com>
2023-04-07 22:12:44 +0100

    fixes

--- Components/Tech.tsx
+++ Components/Tech.tsx
@@ -1,17 +1,19 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Tooltip } from "@chakra-ui/react";
 import Image from "next/image";
 import { motion, spring } from "framer-motion";
 
 const TechImage = (props: any) => {
   return ( 
     <Box width={'fit-content'} objectFit={'contain'}>
-      <Image 
-        src={props.src}
-        alt={props.alt}
-        loading={'lazy'}
-        width={100}
-        height={60}
-      />
+      <Tooltip label={props.tooltip} aria-label='A tooltip' fontFamily={'fantasy'}>
+        <Image 
+          src={props.src}
+          alt={props.alt}
+          loading={'lazy'}
+          width={100}
+          height={60}
+        />
+      </Tooltip>
       </Box>
    );
 }
@@ -24,9 +26,10 @@ const Tech = () => {
       viewport={{ once: true }}
       transition={spring}
     >
-      <Box py={{base:'2.5rem', }}  px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} textAlign={'center'} minHeight={'50vh'} id={'tech'} fontFamily={'fantasy'}>
+      <Box py={{base:'2.5rem', }}  px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} textAlign={'center'}height={{base:'100vh', '2xl':'fit-content'}} id={'tech'}  maxWidth={'1500px'} fontFamily={'fantasy'}>
+
         <Heading textAlign={'center'} fontFamily={'fantasy'}>Tech Stack</Heading>
-        <Text pb={'1rem'}>Tech worked with over time. Skilled at using the following software</Text>
+        <Text pb={'3rem'}>Tech worked with over time. Skilled at using the following software</Text>
 
         <Box 
           display={{base:'grid'}} 
@@ -39,43 +42,53 @@ const Tech = () => {
           <TechImage 
             src='/HTML5.png'
             alt='HTML'
+            tooltip='HyperText Markup Language'
           />
           <TechImage 
             src='/CSS.png'
             alt='CSS'
+            tooltip='Cascading Style Sheet'
           />
           <TechImage 
             src='/JavaScript.png'
             alt='JavaScript'
+            tooltip='JavaScript'
           />
           <TechImage 
           src='/react.png'
           alt='React'
+          tooltip='React'
           />
           <TechImage 
             src='/TypeScript.png'
             alt='TypeScript'
+            tooltip='TypeScript'
           />
           
           <TechImage 
             src='/Nextjs.png'
             alt='Next.JS'
+            tooltip='Next.JS'
           />
           <TechImage 
             src='/redux.png'
             alt='Redux'
+            tooltip='Redux'
           />
           <TechImage 
           src='/tailwind.png'
           alt='Tailwind'
+          tooltip='Tailwind'
           />
           <TechImage 
             src='/mui.png'
             alt='Material UI'
+            tooltip='Material UI'
           />
           <TechImage 
             src='/chakra.png'
             alt='Chakra UI'
+            tooltip='Chakra UI'
           />
           <TechImage 
             src='/bootstrap.png'
