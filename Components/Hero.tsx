c86854930109056a2dee204314d645c08600031d
Jeremy Smith <st.jeremy1@gmail.com>
2023-04-07 21:49:31 +0100

    fixes

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,8 +1,8 @@
 import { Box, Heading, Text, Button } from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import styles from '@/styles/Home.module.css'
-import { MdContacts, MdArrowForward } from "react-icons/md";
-import CustomBtn from "./CustomBtn";
+import styles from '@/styles/Home.module.css';
+import { MdArrowForward } from "react-icons/md";
+import Image from "next/image";
 
 
 const Hero = () => {
@@ -15,16 +15,16 @@ const Hero = () => {
       dragTransition={{ bounceStiffness: 600, bounceDamping: 50 }} 
       dragPropagation
     >
-      <Box px={{base: '1.25rem', sm: '1.7rem', md: '3rem', lg: '6rem'}} pt={{base:'5rem', md:'7rem'}} height={{base:'100vh', lg:'fit-content'}} position={'sticky'} display={'flex'} flexDir={'column'} alignItems={'center'} justifyContent={'center'} textAlign={{base:'center', md: 'left'}} gap={'1rem'} width={{base: '100%', md: '60%'}} maxWidth={'1500px'} id={'hero'}>
+      <Box px={{base: '1.25rem', sm: '1.7rem', md: '3rem', lg: '6rem'}} pt={{base:'5rem', md:'7rem'}} height={{base:'100vh', lg:'fit-content'}} position={'sticky'} display={'flex'} flexDir={'row'} alignItems={'center'} justifyContent={'center'} textAlign={{base:'center', lg: 'left'}} gap={'1rem'} maxWidth={'1500px'} id={'hero'}>
 
-        <Box textAlign={{base:'center', md: 'left'}}>
-          <Heading fontFamily={'fantasy'} fontSize={{base:'24px', lg: '64px'}} pb={5}>Hello, I am</Heading>
+        <Box textAlign={{base:'center', md: 'left'}} width={{base: '100%', lg: '60%'}} >
+          <Heading fontFamily={'fantasy'} fontSize={{base:'24px', md: '40px', lg:'30px'}} pb={5}>Hello, I am</Heading>
 
-          <Heading fontFamily={'fantasy'} fontSize={{base:'57px', lg: '64px'}} color={'blue.900'} textShadow={'-3px -3px blue'} pb={5}>JEREMY SMITH</Heading>
+          <Heading fontFamily={'fantasy'} fontSize={{base:'57px', md: '79px', lg: '64px'}} color={'blue.900'} textShadow={'-3px -3px blue'} pb={5}>JEREMY SMITH</Heading>
           
-          <Heading fontFamily={'fantasy'} fontSize={{base:'20px', lg: '54px'}} pb={5}>a Front-End Developer</Heading>
+          <Heading fontFamily={'fantasy'} fontSize={{base:'20px', md: '40px', lg: '40px'}} pb={5}>a Front-End Developer</Heading>
 
-          <Text fontSize={{base:'16px', lg: '24px'}} mb={'3rem'} textShadow={'outline'}>
+          <Text fontSize={{base:'16px', md:'24px', lg: '24px'}} mb={'3rem'} textShadow={'outline'}>
             ...with a mission of turning designs into great experiences, through user-centric approach.
           </Text>
 
@@ -41,8 +41,17 @@ const Hero = () => {
               </a>
             </Button>
           </motion.button>
-
         </Box>
+
+        <Box display={{base:'none', lg:'flex'}} width={{ lg: '40%'}}>
+          <Image 
+            src='/heroImage.png'
+            height={300}
+            width={500}
+            alt='hero image'
+          />
+        </Box>
+        
       </Box>
 
       <Box position={'absolute'} left={0} top={{base:'29rem'}}>
