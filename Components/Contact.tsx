d13266d4dd578ec2580f8922a38166a10a02af09
Jeremy Smith <st.jeremy1@gmail.com>
2023-04-07 23:55:36 +0100

    fixes

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -1,16 +1,21 @@
 import { Box, Heading, Text, Button } from '@chakra-ui/react';
 import { BsGithub, BsLinkedin, BsMailbox, BsPhone, BsWhatsapp } from 'react-icons/bs';
+import SectionHeading from './SectionHeading';
+import SectionSubHeading from './SectionSubHeading';
 
 const Contact = () => {
   return ( 
-    <Box  px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} py={'9rem 1rem'} textAlign={'center'} id={'contact'}>
+    <Box  px={{base: '1.25rem', sm: '1.5rem', md: '3rem', lg: '6rem'}} py={{base:'4rem', sm:'5rem', md:'5rem' }} textAlign={'center'} id={'contact'}>
 
-      <Heading fontFamily={'fantasy'}>Contact</Heading>
-      <Text >
-        Have an exciting project you need help with or collaboration on?
-      </Text>
-
-      <Text fontWeight={700} m={'20px 0 3px'}>Contact me via </Text>
+      <SectionHeading
+        name='Contact'
+      />
+      <SectionSubHeading
+        name='Have an exciting project you need help with or collaboration on?'
+      />
+      <SectionSubHeading
+        name='Contact me via'
+      />
       
       <Box display={{base:'flex', md: 'grid'}} gridTemplateColumns={'auto auto auto'} flexDir={{ base:'column'}} gap={'10px'} justifyContent={{base:'left', md:'center'}} width={{base:'13.5rem', md: '100%'}} margin={'auto'} alignItems={'center'}>
         <Button leftIcon={<BsMailbox />}>
