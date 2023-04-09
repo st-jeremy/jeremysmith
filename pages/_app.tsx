9fb4574f07ea84bbb19257496b7fc72200ab0085
Jeremy Smith <st.jeremy1@gmail.com>
2023-04-09 13:40:30 +0100

    fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Inter, MuseoModerno } from 'next/font/google'
+
+const inter = MuseoModerno({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    
     <ChakraProvider>
+      <main className={inter.className}>
       <Component {...pageProps} />
+      </main>
     </ChakraProvider>
+    
   )
 }
