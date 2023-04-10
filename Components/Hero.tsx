244415b774b22a4bedf26cec1ea61130fb2f58c2
Jeremy Smith <st.jeremy1@gmail.com>
2023-04-10 19:52:36 +0100

    fixes

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -11,11 +11,17 @@ const Hero = () => {
       <Box px={{base: '1.25rem', sm: '1.7rem', md: '3rem', lg: '6rem'}} pt={{base:'7rem', md:'7rem', lg:'10rem'}} height={{base:'90vh', '2xl':'fit-content'}} position={'sticky'} display={'flex'} flexDir={'row'} alignItems={'center'} justifyContent={'center'} textAlign={{base:'center', lg: 'left'}} gap={'1rem'} maxWidth={'1500px'} background={'linear-gradient( to bottom, white 0%, rgb(184,227,253) 100%)'} margin={'auto'}  id={'hero'}>
         <Box textAlign={{base:'center', lg: 'left'}} width={{base: '100%', lg: '60%'}} >
          
-          <Heading fontFamily={'Verdana, Geneva, Tahoma, sans-serif'} fontSize={{base:'24px', md: '40px', lg:'30px'}} pb={5}>Hello, I am</Heading>
+          <Heading fontFamily={'Verdana, Geneva, Tahoma, sans-serif'} fontSize={{base:'24px', md: '40px', lg:'30px'}} pb={5}>
+            Hello, I am
+          </Heading>
 
-          <Heading fontFamily={'Verdana, Geneva, Tahoma, sans-serif'} fontSize={{base:'68px', md: '80px', lg: '84px'}} color={'blue.900'} fontWeight={900} pb={5}>JEREMY SMITH</Heading>
+          <Heading fontFamily={'Verdana, Geneva, Tahoma, sans-serif'} fontSize={{base:'68px', md: '80px', lg: '84px'}} color={'blue.900'} fontWeight={900} pb={5}>
+            JEREMY SMITH
+          </Heading>
           
-          <Heading  fontFamily={'Verdana, Geneva, Tahoma, sans-serif'} fontSize={{base:'20px', md: '40px', lg: '40px'}} pb={5}>a Front-End Developer</Heading>
+          <Heading  fontFamily={'Verdana, Geneva, Tahoma, sans-serif'} fontSize={{base:'20px', md: '40px', lg: '40px'}} pb={5}>
+            a Front-End Developer
+          </Heading>
 
           <Text fontSize={{base:'16px', md:'24px', lg: '24px'}} mb={'2rem'} textShadow={'outline'}>
             ...with a mission of turning designs into great experiences, through user-centric approach.
@@ -56,7 +62,9 @@ const Hero = () => {
       </Box>
 
       <Box position={'absolute'} left={{base: 0, xl: 5}} top={{base:'34rem', xl: '36rem'}}>
-        <Text className={styles.scrollDown} fontSize={{base:'10.5px', lg: '14px'}} fontWeight={900} padding={1}>SCROLL&nbsp; DOWN</Text>
+        <Text className={styles.scrollDown} fontSize={{base:'10.5px', lg: '14px'}} fontWeight={900} padding={1}>
+          SCROLL&nbsp; DOWN
+        </Text>
       </Box>
     </motion.div>
   );
